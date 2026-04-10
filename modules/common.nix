1ce1d290a4388342b7e77002a357440daf3cbc84
FKouhai <frandres00@gmail.com>
2026-04-10 02:16:58 +0200

    chore: colmena update configs

--- modules/common.nix
+++ modules/common.nix
@@ -56,7 +56,38 @@
     "cgroup_enable=cpuset"
     "cgroup_memory=1"
     "cgroup_enable=memory"
+    "ip_tables"
+    "iptable_nat"
+    "iptable_mangle"
+    "iptable_raw"
+    "iptable_filter"
+    "xt_socket"
   ];
 
-  networking.firewall.enable = false;
+  networking.firewall = {
+    allowedTCPPorts = [
+      80 # HTTP ingress
+      443 # HTTPS ingress
+      4240 # Cilium health checks
+      4244 # Hubble
+      5001 # k3s' embedded Spegel
+      6443 # k3s supervisor; k8s API
+      7946 # MetalLB
+      9100 # Prometheus Node Exporter
+      9962 # cilium-agent metrics
+      9963 # cilium-operator metrics
+      10250 # kubelet metrics
+      2379
+      2380
+
+    ];
+    allowedUDPPorts = [
+      8472 # Cilium VXLAN
+      51871 # Cilium WireGuard
+    ];
+
+    # Reverse-path filtering is discouraged by Cilium.
+    # cilium-1.15.2/pkg/datapath/loader/base.go:365
+    checkReversePath = false;
+  };
 }
