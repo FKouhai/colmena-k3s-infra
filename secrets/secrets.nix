0f683cc7b7e67d3ac15847164d4c18382e28d50f
FKouhai <frandres00@gmail.com>
2026-04-15 21:48:50 +0200

    chore(secrets): fix worker host keys and rekey all secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,9 @@
 let
   epsylon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMv3NiE4yau0yssWjamr1eRSfEXHMlwrnMbQaMLJL7Nw";
-  worker01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzn/nIwm8OkZl0i6WFcyGaOU7pgf9MSyditS9Cb+kGX nixos@worker01";
-  worker02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDXZUmYfQNSgw5XZ/pZaQQtNnqfFrXCyDa9Z4hRFWmOm nixos@worker02";
-  worker03 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMPScx2ZdYk/hx6K34iAdiUyRHamP9FZMvmeVUbhhC+l nixos@worker03";
-  worker04 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDa6oZddfETHuDvGB/rVHYfposYLKEz9wkvObhsRjZnY nixos@worker04";
+  worker01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsodbjN8n5+cGgrzv9CzjfZmNg9SC9cVF+2saOlvT/+ root@nixos";
+  worker02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXozSZ5kELvFtgABUo01zW57a2Fwr77dAi21uffPFai root@nixos";
+  worker03 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFBzNrWVUsFMfIDHWcHKpzPmWr6pGCuyPdLqf7o7mTk0 root@worker03";
+  worker04 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEecocGFnzgG/u/ZR4avX1YC1m7XGYyo0uyILTWz23Ne root@worker04";
   franky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFyWsnvAIM23SRQCW4AIPKeNhVeCWtez/CV1hDegCunC";
 in
 {
